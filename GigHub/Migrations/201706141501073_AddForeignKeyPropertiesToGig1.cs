2d9a5a6b4f05c37ddea43bd9195f7451524151ee
Guillaume BADINIER <g.badinier@hotmail.fr>
2017-06-14 17:22:19 +0200

    GUB - Implement saving a gig

---
+++ GigHub/Migrations/201706141501073_AddForeignKeyPropertiesToGig1.cs
@@ -0,0 +1,18 @@
+namespace GigHub.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class AddForeignKeyPropertiesToGig1 : DbMigration
+    {
+        public override void Up()
+        {
+
+        }
+        
+        public override void Down()
+        {
+
+        }
+    }
+}
